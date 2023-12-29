7b2bc19ed10481e1fda361bebd6891ba7417f8b1
Kaifan Zheng <77627965+kaifanzheng@users.noreply.github.com>
2023-12-29 08:26:20 -0500

    add scroll to top function, update contact language
    add scroll to top function, update contact language

--- src/App.js
+++ src/App.js
@@ -5,17 +5,30 @@ import SecondPage from './components/SecondPage/SecondPage';
 import TeamMemberComponent from './components/TeamMember/TeamMemberComponent';
 import FooterComponent from './components/FooterComponent/FooterComponent';
 import ContactPage from './components/ContactPage/ContactPage';
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { useState ,useEffect} from 'react';
+import { BrowserRouter as Router, Route, Routes,useLocation } from 'react-router-dom';
+
+
 function App() {
   const [language, setLanguage] = useState('EN'); // Start with 'EN' for English
 
   const toggleLanguage = () => {
     setLanguage(language === 'EN' ? 'CN' : 'EN'); // Toggle between 'EN' and 'CN'
   };
+
+  const ScrollToTop = () => {
+    const location = useLocation();
+  
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [location]);
+  
+    return null;
+  };
   return (
     <div className="App">
       <Router basename="/Kinesiolog-Y-Startup-Website">
+        <ScrollToTop />
         <Header language={language} toggleLanguage={toggleLanguage} />
         <Routes>
           <Route path="/" element={
@@ -26,7 +39,7 @@ function App() {
             <FooterComponent language={language} />
             </>
           } />
-          <Route path="/contact" element={<> <ContactPage /> <FooterComponent language={language} /></>} />
+          <Route path="/contact" element={<> <ContactPage language={language}/> <FooterComponent language={language} /></>} />
           {/* Define other routes here */}
         </Routes>
       </Router>
--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import './ContactPage.css'; // Make sure to create and import the corresponding CSS file
 import qrCodeImage from '../../assets/qr-code.jpg'; // Adjust the path to your QR code image
 
-const ContactPage = () => {
+const ContactPage = ({ language }) => {
   return (
     <div className="contact-page">
-      <h1>Contact Us</h1>
-      <p>Email: <a href="mailto:kinesio.logyyyy@gmail.com">kinesio.logyyyy@gmail.com</a></p>
+      <h1>{language === 'EN' ? 'Contact Us' : '联系我们'}</h1>
+      <p>{language === 'EN' ? 'Email: ' : '邮箱：'}<a href="mailto:kinesio.logyyyy@gmail.com">kinesio.logyyyy@gmail.com</a></p>
       <div className="qr-code-container">
         <img src={qrCodeImage} alt="QR Code" />
       </div>
