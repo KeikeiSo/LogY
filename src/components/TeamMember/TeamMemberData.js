4b69c993b70b239ee8c8752a97a5f8105b996cf0
Kaifan Zheng <kaifan.zheng@mail.mcgill.ca>
2024-02-16 05:05:34 -0500

    remove redundancy

--- src/components/TeamMember/TeamMemberData.js
+++ src/components/TeamMember/TeamMemberData.js
@@ -17,15 +17,15 @@ const teamMembers = [
       description: "Ruoyan brings a wealth of knowledge in musculoskeletal health. She can empower individuals toward higher performance levels with her expertise in Valencia Soccer Club, injury prevention, and rehabilitation.",
       descriptionCn: "Ruoyan在肌肉骨骼健康方面拥有丰富的知识。她凭借对瓦伦西亚足球俱乐部、预防伤病和康复的专业知识，能够帮助个人实现更高水平的表现。",
       image: ry // image path
-    },
-    {
-      name: "Erin Ma",
-      role: "Certified Massage Therapist (AMQ)",
-      roleCn: "AMQ认证按摩治疗师",
-      description: "Erin merges her passion for therapeutic touch with a profound understanding of the human body anatomy. Her knowledge of boxing and Chinese traditional Bian stone therapy allows her to provide professional services to enhance one’s overall well-being and health.",
-      descriptionCn: "Erin将她对治疗性触摸的热情与对人体解剖学的深刻理解融合在一起。她对拳击和中国传统的砭石疗法的了解使她能够提供专业服务，以提升个人的整体健康",
-      image: e // image path
     }
+    // {
+    //   name: "Erin Ma",
+    //   role: "Certified Massage Therapist (AMQ)",
+    //   roleCn: "AMQ认证按摩治疗师",
+    //   description: "Erin merges her passion for therapeutic touch with a profound understanding of the human body anatomy. Her knowledge of boxing and Chinese traditional Bian stone therapy allows her to provide professional services to enhance one’s overall well-being and health.",
+    //   descriptionCn: "Erin将她对治疗性触摸的热情与对人体解剖学的深刻理解融合在一起。她对拳击和中国传统的砭石疗法的了解使她能够提供专业服务，以提升个人的整体健康",
+    //   image: e // image path
+    // }
   ];
   
   export default teamMembers;
